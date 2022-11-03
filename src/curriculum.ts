ce421a79b13b904691bc31032ac71ca9a02ec8d4
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-11-03 14:13:27 +0100

    Avanzo #16 porque se ha reimplementado el concepto de Curriculum
    Con estos cambios se responde a los problemas planteados en #16

--- src/curriculum.ts
+++ src/curriculum.ts
@@ -1,4 +1,4 @@
-enum nivelIngles {
+export enum nivelIngles {
   A1 = "A1",
   A2 = "A2",
   B1 = "B1",
@@ -8,7 +8,7 @@ enum nivelIngles {
 };
 
 
-enum nivelEstudios {
+export enum nivelEstudios {
   Primaria = "primaria",
   Secundaria = "secundaria",
   Bachillerato = "bachillerato",
@@ -17,18 +17,52 @@ enum nivelEstudios {
   Master = "master"
 };
 
+
+
 export class Curriculum{
   
   private carnetConducir:boolean;
   private ingles:nivelIngles;
   private estudios:nivelEstudios;
   private aptitudes:string[];
+  private experiencia:number;
 
-  constructor(carnetConducir:boolean,ingles:nivelIngles,estudios:nivelEstudios,aptitudes:string[]){
+
+  constructor(carnetConducir:boolean,ingles:nivelIngles,estudios:nivelEstudios,aptitudes:string[],experiencia:string){
     this.ingles = ingles;
     this.estudios = estudios;
     this.carnetConducir = carnetConducir;
     this.aptitudes = aptitudes;
+    this.experiencia = this.obtenerExperiencia(experiencia);
+  }
+
+  obtenerExperiencia(experiencia:string):number {
+    if((Number(experiencia) >=0) && (Number(experiencia)<35)){
+      return Number(experiencia);
+    }else{
+      throw new Error("Valor de experiencia no válido");
+    }
+  }
+
+  getExperiencia():number {
+    return this.experiencia;
+  }
+
+  getAptitudes():string[]{
+    return this.aptitudes;
+  }
+
+  getCarnetConducir():boolean{
+    return this.carnetConducir;
+  }
+
+  getEstudios():nivelEstudios{
+    return this.estudios;
+  }
+
+  getIngles():nivelIngles{
+    return this.ingles;
   }
 
 }
+
