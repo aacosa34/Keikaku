d7ee9a56b50566cebf40c1c3539af193feb0259a
MarinoFajardo <marinofajardo@correo.ugr.es>
2022-11-08 14:00:14 +0100

    chore(src/curriculum.ts)#21: Added interface Experiencia Se ha creado la interfaz experiencia en el mismo formato que en Europass

--- src/curriculum.ts
+++ src/curriculum.ts
@@ -1,63 +1,14 @@
-export enum nivelIngles {
-  A1 = "A1",
-  A2 = "A2",
-  B1 = "B1",
-  B2 = "B2",
-  C1 = "C1",
-  C2 = "C2"
-};
+interface Experiencia{
+  fechaInicio: Date;
+  fechaFin: Date;
+  Puesto: string;
+}
 
+export class Curriculum{ 
+  private experiencias : Experiencia[];
 
-export enum nivelEstudios {
-  Primaria = "primaria",
-  Secundaria = "secundaria",
-  Bachillerato = "bachillerato",
-  FP = "fp",
-  Grado = "grado",
-  Master = "master"
-};
-
-
-
-export class Curriculum{
-  
-  private ingles:nivelIngles;
-  private estudios:nivelEstudios;
-  private aptitudes:string[];
-  private experiencia:number;
-
-
-  constructor(ingles:nivelIngles,estudios:nivelEstudios,aptitudes:string[],experiencia:string){
-    this.ingles = ingles;
-    this.estudios = estudios;
-    this.aptitudes = aptitudes;
-    this.experiencia = this.obtenerExperiencia(experiencia);
+  constructor(exp : Experiencia[]){
+    this.experiencias = exp;
   }
-
-  obtenerExperiencia(experiencia:string):number {
-    if((Number(experiencia) >=0) && (Number(experiencia)<35)){
-      return Number(experiencia);
-    }else{
-      throw new Error("Valor de experiencia no válido");
-    }
-  }
-
-  getExperiencia():number {
-    return this.experiencia;
-  }
-
-  getAptitudes():string[]{
-    return this.aptitudes;
-  }
-
-  getEstudios():nivelEstudios{
-    return this.estudios;
-  }
-
-  getIngles():nivelIngles{
-    return this.ingles;
-  }
-  
-
 }
 
