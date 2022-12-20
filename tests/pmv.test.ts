5e9516c2ad62d42981500db97ae1f19602857d35
Adrian Acosa Sanchez <adri110301@gmail.com>
2022-12-20 11:44:51 +0100

    test(#37): tests reading of example file

--- tests/pmv.test.ts
+++ tests/pmv.test.ts
@@ -1,9 +1,21 @@
 import { Curriculum } from '../src/curriculum';
 import { Oferta } from '../src/oferta';
+import { readFileSync } from 'fs';
 
-test("Dummy test", () => {
-    expect(true).toBe(true);
+describe("Test de prueba", () => {
+    test("Dummy test", () => {
+        expect(true).toBe(true);
+    })
 })
 
+describe("Tests del PMV", () => {
+    let result : string;
+    // Inicialización de las variables necesarias para ejecutar los tests
+    beforeAll(() => {
+        result = readFileSync('data/data.csv', 'utf8');
+    })
 
-
+    test("Lectura correcta del CSV", () => {
+        expect(result).not.toBe("");
+    })
+})
